ff1d154fd847d0b57001a016fb20ded95f402bdd
vidhyasakthi <149054459+vidhyasakthi@users.noreply.github.com>
2025-08-25 07:19:34 +0530

    index.js

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const nodemailer = require("nodemailer");
 const mongoose = require("mongoose");
-require("dotenv").config();  // ✅ load env variables
+require("dotenv").config();  
 
 const app = express();
 app.use(cors());
